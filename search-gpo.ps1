fc82205c2ab7fd05268fd464dace098239e9f52a
Will McVicker <thegpfury@outlook.com>
2018-05-13 00:26:33 -0400

    Added CSV export, added OU details for each GPO with inheritance

--- search-gpo.ps1
+++ search-gpo.ps1
@@ -13,7 +13,10 @@ param (
 [array]$reglist = @("ProxyOverride","AutoConfigURL","ProxyServer"),
 # This uses colon separated values to store a GPO policy that has an affiliated setting
 # Yes, this is ugly. But, it works. 
-[array]$policyedlist = @("Force specific screen saver:edittext:value","Screen saver Timeout:numeric:value","Hide These Specified Drives in My Computer:Dropdownlist:Value")
+[array]$policyedlist = @("Force specific screen saver:edittext:value","Screen saver Timeout:numeric:value","Hide These Specified Drives in My Computer:Dropdownlist:Value"),
+# Export CSV, yes/no, and where to put it
+[bool]$exportCSV = 1,
+[string]$csvpath = "C:\pstemp\gpreport.csv"
 )
 
 function GrabFavorites($faves)
@@ -42,93 +45,108 @@ if($getgpo = 1)
 # Loads all xml from the path
 [xml]$all = get-content -Path $xmlpath
 
-# Starts going through each GPO
-foreach($gpo in $all.report.gpo)
+# Gets the OU Structure
+$OuList = get-adorganizationalunit -filter *
+
+foreach($ou in $oulist)
 {
-    # PSObject for each GPO
-    $array = New-Object PSObject
-    Add-Member -inputObject $array -memberType NoteProperty -name “GPO” -value $gpo.name
-
-    # Check for the registry settings
-    foreach($reg in $reglist)
-    {
-    $registryset = $gpo.user.ExtensionData.extension.registrysettings.collection.collection.collection.collection.collection.collection.collection.registry.properties | where {$_.name -eq $reg}   
-    
-    if ($registryset)
+    $OuGPO = Get-GPInheritance -target $ou.DistinguishedName
+    $targetedGPO = $all.report.gpo | Where-Object {$_.name -in $ougpo.InheritedGpoLinks.displayname}
+        # Starts going through each GPO
+        foreach($gpo in $targetedGPO)
         {
-        if($registryset.action -ne "D")
+            # PSObject for each GPO
+            $array = New-Object PSObject
+            $ouProperName = ($ou.DistinguishedName -replace "OU=","" -replace "DC=(.*)","").Split(",")
+            [array]::reverse($ouProperName)
+            $oupropernamefixed = $oupropername -join "\"                        
+            Add-Member -inputObject $array -memberType NoteProperty -name "OU" -value $oupropernamefixed
+            Add-Member -inputObject $array -memberType NoteProperty -name “GPO” -value $gpo.name
+
+            # Check for the registry settings
+            foreach($reg in $reglist)
             {
-                Add-Member -inputObject $array -memberType NoteProperty -name $reg -value $registryset.value
+            $registryset = $gpo.user.ExtensionData.extension.registrysettings.collection.collection.collection.collection.collection.collection.collection.registry.properties | where {$_.name -eq $reg}   
+            
+            if ($registryset)
+                {
+                if($registryset.action -ne "D")
+                    {
+                        Add-Member -inputObject $array -memberType NoteProperty -name $reg -value $registryset.value
+                    }
+                }
+            else
+                {
+                Add-Member -inputObject $array -memberType NoteProperty -name $reg  -value "N/A"
+                }
+            }   
+            
+            # Check for policys
+            foreach($policy in $policylist)
+            {
+                $currentpolicy = $gpo.user.extensiondata.extension.policy | where {$_.name -eq "$policy"}
+                if($currentpolicy)
+                {
+                    Add-Member -inputObject $array -memberType NoteProperty -name $policy -value $currentpolicy.state
+                }
+                else
+                {
+                    Add-Member -inputObject $array -memberType NoteProperty -name "$policy" -value "N/A"
+                }
+            }   
+            # Check for PAC file -> IE Setting
+            $pacIE = $gpo.user.extensiondata.extension.automaticconfiguration.proxyurl
+            if ($pacIE)
+            {
+                Add-Member -inputObject $array -memberType NoteProperty -name “PacIE” -value $pacIE
             }
-        }
-    else
-        {
-        Add-Member -inputObject $array -memberType NoteProperty -name $reg  -value "N/A"
-        }
-    }   
-    
-    # Check for policys
-    foreach($policy in $policylist)
-    {
-        $currentpolicy = $gpo.user.extensiondata.extension.policy | where {$_.name -eq "$policy"}
-        if($currentpolicy)
-        {
-            Add-Member -inputObject $array -memberType NoteProperty -name $policy -value $currentpolicy.state
-        }
-        else
-        {
-            Add-Member -inputObject $array -memberType NoteProperty -name "$policy" -value "N/A"
-        }
-    }   
-    # Check for PAC file -> IE Setting
-    $pacIE = $gpo.user.extensiondata.extension.automaticconfiguration.proxyurl
-    if ($pacIE)
-    {
-        Add-Member -inputObject $array -memberType NoteProperty -name “PacIE” -value $pacIE
-    }
-    else
-    {
-        Add-Member -inputObject $array -memberType NoteProperty -name “PacIE” -value "N/A"
-    }
-
-    # Check for policies with values 
-    # Ugly hacks ahead!
-    foreach($policyed1 in $policyedlist)
-    {
-        $edpolname, $edvalue, $edpolicy = ($policyed1 -split(':'))
-        $edname = "$edpolname $edvalue $edpolicy"
-        $currentpolicyed = $gpo.user.extensiondata.extension.policy | where {$_.name -eq $edpolname}
-        if ($currentpolicyed)
-        {
-                  
-            Add-Member -inputObject $array -memberType NoteProperty -name $edpolname -value $currentpolicyed.State
-            # Ugly Hack to deal with the 4th point in dropdown list. Gonna see if this breaks more
-            if($edvalue -like "DropDownList")
+            else
             {
-            Add-Member -inputObject $array -memberType NoteProperty -name $edname -value $currentpolicyed.$edvalue.$edpolicy.name
-            }    
-            else 
-            {
-                Add-Member -inputObject $array -memberType NoteProperty -name $edname -value $currentpolicyed.$edvalue.$edpolicy
+                Add-Member -inputObject $array -memberType NoteProperty -name “PacIE” -value "N/A"
             }
-        }
-        else
-        {
-            Add-Member -inputObject $array -memberType NoteProperty -name $edpolname -value "N/A"
-            Add-Member -inputObject $array -memberType NoteProperty -name $edname  -value "N/A"
-        }
-    }
-    # Check for Faves
-    $favearray1 = grabfavorites($gpo.user.extensiondata.extension.favoriteurl)
-    if ($favearray1)
-    {
-        Add-Member -inputObject $array -memberType NoteProperty -name “FaveList” -value $favearray1
-    }
-    else
-    {
-        Add-Member -inputObject $array -memberType NoteProperty -name “FaveList” -value "N/A"   
-    }   
-$totalArray += $array 
-}
 
+            # Check for policies with values 
+            # Ugly hacks ahead!
+            foreach($policyed1 in $policyedlist)
+            {
+                $edpolname, $edvalue, $edpolicy = ($policyed1 -split(':'))
+                $edname = "$edpolname $edvalue $edpolicy"
+                $currentpolicyed = $gpo.user.extensiondata.extension.policy | where {$_.name -eq $edpolname}
+                if ($currentpolicyed)
+                {
+                        
+                    Add-Member -inputObject $array -memberType NoteProperty -name $edpolname -value $currentpolicyed.State
+                    # Ugly Hack to deal with the 4th point in dropdown list. Gonna see if this breaks more
+                    if($edvalue -like "DropDownList")
+                    {
+                    Add-Member -inputObject $array -memberType NoteProperty -name $edname -value $currentpolicyed.$edvalue.$edpolicy.name
+                    }    
+                    else 
+                    {
+                        Add-Member -inputObject $array -memberType NoteProperty -name $edname -value $currentpolicyed.$edvalue.$edpolicy
+                    }
+                }
+                else
+                {
+                    Add-Member -inputObject $array -memberType NoteProperty -name $edpolname -value "N/A"
+                    Add-Member -inputObject $array -memberType NoteProperty -name $edname  -value "N/A"
+                }
+            }
+            # Check for Faves
+            $favearray1 = grabfavorites($gpo.user.extensiondata.extension.favoriteurl)
+            if ($favearray1)
+            {
+                Add-Member -inputObject $array -memberType NoteProperty -name “FaveList” -value $favearray1
+            }
+            else
+            {
+                Add-Member -inputObject $array -memberType NoteProperty -name “FaveList” -value "N/A"   
+            }   
+        $totalArray += $array 
+        }
+    }
 $totalArray | out-gridview
+if($exportcsv -eq 1)
+{
+    $totalarray | export-csv -path $csvpath
+}
